c2eb30d0cd2d281d787ee3292416b60fef418669
Ryan Wooff <ryanwooff@gmail.com>
2018-12-15 17:17:41 -0800

    Update init.js

--- img-slider/js/init.js
+++ img-slider/js/init.js
@@ -1,18 +1,17 @@
 function slide(n){
   var imgNames = ["null", "img_1.jpeg", "img_2.jpeg", "img_3.jpeg", "img_4.jpeg"];
-  var imgName = $('div#slider.slider img.slider-img')[0].outerHTML.split('img_');
+  var selector = 'div#slider.slider img.slider-img';
+  var imgName = $(selector)[0].outerHTML.split('img_');
+  var imgNum = parseInt(imgName[1].split('.')[0]);
   if (n == 0) {
-    if (parseInt(imgName[1].split('.')[0]) != 1){
-      $('div#slider.slider img.slider-img')[0].outerHTML = imgName[0] + imgNames[parseInt(imgName[1].split('.')[0]) - 1] + "\">";
-    } else{
-      $('div#slider.slider img.slider-img')[0].outerHTML = imgName[0] + imgNames[parseInt(imgName[1].split('.')[0]) + 3] + "\">";
-    }
+    var newName = (imgNum != 1) ? imgNames[imgNum - 1] : imgNames[imgNum + 3];
+    $(selector)[0].outerHTML = getPath(imgName[0], newName);
   }
   else if (n == 1 ){
-    if (parseInt(imgName[1].split('.')[0]) != 4){
-      $('div#slider.slider img.slider-img')[0].outerHTML = imgName[0] + imgNames[parseInt(imgName[1].split('.')[0]) + 1] + "\">";
-    } else{
-      $('div#slider.slider img.slider-img')[0].outerHTML = imgName[0] + imgNames[parseInt(imgName[1].split('.')[0]) - 3] + "\">";
-    }
+    var newName = (imgNum != 4) ? imgNames[imgNum + 1] : imgNames[imgNum - 3];
+    $(selector)[0].outerHTML = getPath(imgName[0], newName);
   }
+}
+function getPath(path, imgName){
+  return (path + imgName + "\">");
 }
